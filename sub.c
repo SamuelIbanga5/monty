52b4cbbaa723ea0b67dd8d0b9448a3f7bc65baaa
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-16 01:36:54 +0100

    Updated sub.c file

--- sub.c
+++ sub.c
@@ -11,8 +11,12 @@ void f_sub(stack_t **head, unsigned int counter)
 	int sus, nodes;
 
 	aux = *head;
+
 	for (nodes = 0; aux != NULL; nodes++)
+	{
 		aux = aux->next;
+	}
+
 	if (nodes < 2)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
