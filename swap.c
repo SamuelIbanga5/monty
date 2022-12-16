edc39b2475748ce277a52474fec002377805dd84
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-16 01:37:59 +0100

    UPdated swap.c

--- swap.c
+++ swap.c
@@ -11,11 +11,12 @@ void f_swap(stack_t **head, unsigned int counter)
 	int len = 0, aux;
 
 	h = *head;
-	while (h)
+	while (h != NULL)
 	{
 		h = h->next;
 		len++;
 	}
+
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
