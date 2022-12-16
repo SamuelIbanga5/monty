3e704eb731108eeeece7df119505ff3cf2a4c8c2
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-16 01:34:30 +0100

    Updated rotr.c file

--- rotr.c
+++ rotr.c
@@ -10,14 +10,17 @@ void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
 	stack_t *copy;
 
 	copy = *head;
+
 	if (*head == NULL || (*head)->next == NULL)
 	{
 		return;
 	}
+
 	while (copy->next)
 	{
 		copy = copy->next;
 	}
+
 	copy->next = *head;
 	copy->prev->next = NULL;
 	copy->prev = NULL;
