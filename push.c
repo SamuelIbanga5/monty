1c231ea73fff37312d4c976516fca91746880102
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-16 01:21:10 +0100

    Updated push.c file

--- push.c
+++ push.c
@@ -7,16 +7,23 @@
 */
 void f_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0, flag = 0;
+	int n;
+	int j = 0;
+	int flag = 0;
 
 	if (bus.arg)
 	{
 		if (bus.arg[0] == '-')
+		{
 			j++;
+		}
 		for (; bus.arg[j] != '\0'; j++)
 		{
 			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
+			{
+				flag = 1;
+			}
+		}
 		if (flag == 1)
 		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
